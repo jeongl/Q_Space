41ca010b0f589e302562aea86e750c27dba08172
= <=>
2014-04-16 17:35:12 -0700

    first commit

---
+++ app/Views/ListView.js
@@ -0,0 +1,25 @@
+define(function(){
+
+  function render(parameters){
+
+    var appDiv = document.getElementById('app');
+    var users = parameters.users;
+
+    console.log(users);
+    var html = '<ul>';
+    for (var i = 0, len = users.length; i < len; i++){
+      html+= '<li>' + users[i].name + '</li>';
+    }
+    html+= '</ul>'
+    appDiv.innerHTML= html;
+
+    appDiv.onclick = function() {
+      alert('here!');
+    }
+  }
+
+  return {
+    render : render
+  }
+
+});
---
+++ server.js
@@ -0,0 +1,13 @@
+var express        = require('express');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var app            = express();
+
+app.use(express.static(__dirname));
+app.use(morgan('dev')); 					// log every request to the console
+app.use(bodyParser()); 						// pull information from html in POST
+app.use(methodOverride()); 					// simulate DELETE and PUT
+
+app.listen(3000);
+console.log('Check port 3000');
