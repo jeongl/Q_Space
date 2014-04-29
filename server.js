f035e91fda4951bee76007c555a53899f5ba61e1
lijmLar <jlim8612@gmail.com>
2014-04-29 10:44:13 -0700

    more parse

--- server.js
+++ server.js
@@ -5,17 +5,19 @@ var methodOverride = require('method-override');
 var app            = express();
 var request        = require('request');
 var cheerio        = require('cheerio');
+var async          = require('async');
 
 app.use(express.static(__dirname));
 app.use(morgan('dev')); 					// log every request to the console
 app.use(bodyParser()); 						// pull information from html in POST
 app.use(methodOverride()); 					// simulate DELETE and PUT
 
-console.log('is!');
 app.get('/getQuotes', function(req,res){
+  var temp=[];
+
   request('http://www.brainyquote.com/quotes/favorites', function(err, response, body){
-    var temp=[];
     var $ = cheerio.load(body);
+
     $('div.bq_s a').map(function(index, el){
       if(String($(this).attr('href')).split('/').length === 5){
         temp.push({
@@ -31,16 +33,33 @@ app.get('/getQuotes', function(req,res){
         });
       }
     });
+    processASYNC(
+        (function() {
+          var temp1=[];
 
-//    console.log($);
+          for (var i=0; i<5; i++){
+            var random = Math.floor(Math.random()*temp.length+1)
+            var split = JSON.stringify(temp[random].link).split('/')[4];
+            split = split.split('.')[0];
+            temp1.push(split);
+          }
+          return temp1;
+        }())
+    );
+  });
 
-    res.send(temp);
-  })
+
+  function processASYNC(insert) {
+    console.log(insert);
+    async.forEach(temp, function(item, callback){
+//      console.log(item.link);
+    });
+  }
 
 
   var quotes = [
       {
-        "Person" : "Thomas Edison",
+        "Person" : "Unknown",
         "Quote" : "You can't always get what you want!"
       },
       {
