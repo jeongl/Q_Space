29a702982e0f0dd11be7dd0b5f1dd90a329e4876
jeong <jlim8612@gmail.com>
2014-09-20 08:41:26 -0700

    Change DOM structure, add Class

--- app/Views/IndexView.js
+++ app/Views/IndexView.js
@@ -2,10 +2,8 @@ define(['hbs!assets/templates/IndexView'],function(indexViewTempl) {
 
 
   function render(){
-    var div = document.createElement('div');
-    div.classList.add('Main');
-    div.innerHTML = indexViewTempl();
-    document.getElementsByTagName('body')[0].appendChild(div);
+  	var selector = document.querySelector('.Main');
+  	selector.innerHTML = indexViewTempl();
   }
 
 
