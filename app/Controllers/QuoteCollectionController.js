1b6a1ddf1c4086a12e36b7cfc5d09010dd16b401
jeong <jlim8612@gmail.com>
2014-09-29 00:09:17 -0700

    Add forms

--- app/Controllers/QuoteCollectionController.js
+++ app/Controllers/QuoteCollectionController.js
@@ -37,11 +37,10 @@ define(['Views/QuoteCollectionView', 'Util/Spin', 'Models/SaveVote' ],function(Q
     fn.attachUpVoteHandler = function() {
       var self = this;
       $('.VoteButton').on('click', function(e) {
-        window.el = $(e.currentTarget).parentsUntil('.col-xs-8').eq(2);   
+        var el = $(e.currentTarget).parentsUntil('.col-xs-8').eq(2);   
         var id = el.parent().find('.VoteIcons').find('button').attr('id');
         var voteNum = $(el).find('.NumVotes').html();
-        var offset = /VoteButton Up/.test( $(e.currentTarget)[0].className) ? 1:-1;
-        console.log('el: ', el);
+        var offset = /VoteButton Up/.test( $(e.currentTarget)[0].className) ? 1:-1;        
         var saveObj = {
           id : id,
           num :  Number(voteNum) + Number(offset),
@@ -50,7 +49,6 @@ define(['Views/QuoteCollectionView', 'Util/Spin', 'Models/SaveVote' ],function(Q
         self.saveToDB(saveObj, {
           success: function(response) {
             $(el).find('.NumVotes').html( saveObj.num  );
-            // console.log('response after saving: ', response);a
           },
           fail: function(response) {
             console.log('Failed response: ', response);
--- app/Views/IndexView.js
+++ app/Views/IndexView.js
@@ -2,8 +2,12 @@ define(['hbs!assets/templates/IndexView'],function(indexViewTempl) {
 
 
   function render(){
+  	var date = new Date();
   	var selector = document.querySelector('.Main');
-  	selector.innerHTML = indexViewTempl();
+  	// console.log('Date: ', date.getUTCMonth(), date.getUTCDate(), date.getUTCFullYear() );
+  	date =  date.getUTCMonth()+1 + '-' + date.getUTCDate() + '-' + date.getUTCFullYear();
+  	console.log(date)
+  	selector.innerHTML = indexViewTempl({Date:date});
   }
 
 
